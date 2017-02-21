5f20e3fad65b15ad40ea1f4e031c32a7b704c6e8
Simon Dell <simon@expressionworks.co.uk>
2017-02-21 19:08:28 +0000

    Refactor the Metrics component to use the data keys

--- src/components/Metric.js
+++ src/components/Metric.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Metric extends React.Component {
 	render () {
 		return (
-			<div>{ this.props.title }: { this.props.value }</div>
+			<div>{ this.props.title.replace('-', ' ') }: { this.props.value }</div>
 		);
 	}
 }
