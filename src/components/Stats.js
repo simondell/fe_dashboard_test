eadd69406340fe24b81a689261e787122b0bf765
Simon Dell <simon@expressionworks.co.uk>
2017-02-21 19:09:02 +0000

    Refactor to map data -> metrics, rather than explicitly defining each one

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,15 +3,16 @@ import React from 'react';
 import SalesSummary from './SalesSummary';
 
 export default class Stats extends React.Component {
+	getMetrics () {
+		this.props.data.measures.map( m => console.log( m ) );
+		return this.props.data.measures.map( m => <Metric key={ m.title } title={ m.title } value={ m.value } /> );
+	}
+
 	render () {
 		return (
 			<section>
 				<SalesSummary />
-				<Metric title="Vehicles made" value="23" />
-				<Metric title="Retail orders" value="23" />
-				<Metric title="Online orders" value="23" />
-				<Metric title="Configurations completed" value="23" />
-				<Metric title="Test drives arranged" value="23" />
+				{ this.getMetrics() }
 			</section>
 		);
 	}
