77deafc8689e1975a4e1be3a798944883276efea
Simon Dell <simon@expressionworks.co.uk>
2017-02-21 16:33:52 +0000

    Added react
    Update build to process JSX
    
    Added a dumb app component to hook others onto

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-let message = `
+import App from './components/app';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(`
 ############################
 # Dashboard technical test #
 # by Simon Dell            #
 ############################
-`;
+`);
 
-console.log( message );
-
-var title = document.createElement('h1');
-title.innerText = 'Sales vs Target for Week 3';
-document.querySelector('body').appendChild( title );
-
-
+ReactDOM.render(
+	<App/>,
+	document.querySelector('body') // React complains about this, but its main objection doesn't apply when we control all the nodes
+);
---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <main>
+    <header><h1>Sales vs Target for Week 3</h1></header>
+  </main>
+);
+
+export default App;
