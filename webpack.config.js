681996fbe6e9e64598cfae4189499b3d8659b84d
Simon Dell <simon@expressionworks.co.uk>
2017-02-21 13:18:57 +0000

    Added babel and config
    Fixed webpack config object syntax
    
    There's still an issue around babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ var path = require('path');
 var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: './src/app.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		}]
+	}
 };
