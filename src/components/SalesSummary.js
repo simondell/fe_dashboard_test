3cf26d4c1cc78c1fa8d13785bc5eee56034ba059
Simon Dell <simon@expressionworks.co.uk>
2017-02-21 17:43:45 +0000

    Added a title

--- src/components/SalesSummary.js
+++ src/components/SalesSummary.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class SalesSummary extends React.Component {
 	render () {
 		return (
-			<div>Graphs go here</div>
+			<section>
+				<h1>Sales vs Target for Week 3</h1>
+				<p>Graphs go here</p>
+			</section>
 		);
 	}
 }
