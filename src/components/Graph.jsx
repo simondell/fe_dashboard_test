db5b83bff7f72b43887c795601e858f332661640
Simon Dell <simon@expressionworks.co.uk>
2017-02-22 12:49:57 +0000

    Proving data gets to the graph component

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Graph extends React.Component {
 	render () {
-		return <div>Here be a graph</div>;
+		return <div>The graph will have { this.props.data.length } data points</div>;
 	}
 }
