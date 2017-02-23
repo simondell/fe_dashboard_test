51aa99bf95da7ff902db39f16bbf688fd09af361
Simon Dell <simon@expressionworks.co.uk>
2017-02-23 13:43:20 +0000

    Layout looking good #12

--- src/components/number/number.jsx
+++ src/components/number/number.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-export default class Number extends React.Component {
+export default class FormattedNumber extends React.Component {
 	constructor ( props ) {
 		super( props );
 		// TODO: refactor to format number
-		this.state = { display: props.value };
+		this.state = { display: this.truncate( props.value ) };
 	}
 
 	render () {
 		return <span>{ this.state.display }</span>;
+	}
+
+	thousends ( value ) {
+		return `${( value / 1000 ).toPrecision( 3 )}k`;
+	}
+
+	truncate ( value ) {
+		if( value > 10000 ) return this.thousends( value );
+		if( value > 1000 ) return this.withComma( value );
+		else return value;
+	}
+
+	withComma ( value ) {
+		let text = value.toString();
+		let len = text.length;
+		return `${text.charAt(0)},${text.slice(1)}`;
 	}
 }
