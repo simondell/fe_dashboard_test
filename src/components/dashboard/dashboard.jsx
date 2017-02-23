d3be8aa19b5cd2ab0900eb0b58f81091c4981a78
Simon Dell <simon@expressionworks.co.uk>
2017-02-23 12:44:45 +0000

    Metrics now a table
    changed store data structure to support metrics rather than measures
    
    Refactored components to better describe structure

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,20 +1,15 @@
 import Metric from '../metric/metric';
 import React from 'react';
+import Stats from '../stats/stats';
 import SalesSummary from '../sales_summary/sales_summary';
 import styles from './dashboard.css';
 
 export default class Dashboard extends React.Component {
-	getMetrics () {
-		return this.props.data.measures.map( m => {
-			return <Metric key={ m.title } title={ m.title } value={ m.value } />;
-		});
-	}
-
 	render () {
 		return (
 			<main className={ styles.dashboard }>
 				<SalesSummary data={ this.props.data } />
-				{ this.getMetrics() }
+				<Stats metrics={ this.props.data.metrics } />
 			</main>
 		);
 	}
