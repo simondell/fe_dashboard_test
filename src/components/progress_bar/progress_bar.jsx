c100eab0a091bcb223149aceea4bd4467d12df0e
Simon Dell <simon@expressionworks.co.uk>
2017-02-23 18:01:13 +0000

    Progress bar layout #8
    Could do with some clevers to deal with clashes between the max and min labels

--- src/components/progress_bar/progress_bar.jsx
+++ src/components/progress_bar/progress_bar.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
 import Number from '../number/number';
+import React from 'react';
+import styles from './progress_bar.css';
 
 export default class ProgressBar extends React.Component {
+	constructor ( props ) {
+		super( props );
+
+		let { now, max } = props;
+		let split = Math.floor( now / max * 100 );
+		this.state = { split };
+		console.log( this.state );
+	}
+
+	getMaxWidth () {
+		let width = `${ 100 - this.state.split }%`;
+		return { width };
+	}
+
+	getNowWidth () {
+		let width = `${ this.state.split }%`;
+		return { width };
+	}
+
 	render () {
 		return (
-			<div role="progressbar"
+			<div className={ styles.progressbar }
+				role="progressbar"
 				aria-valuenow={ this.props['now-label'] }
 				aria-valuemin="0"
 				aria-valuemax={ this.props['max'] }
 				aria-valuetext={ this.props['max-label'] }
 			>
-				<span className="now">
-					<span className="now-label">{ this.props['now-label'] }</span>
-					<span className="now-value"><Number value={ this.props['now'] } /></span>
-				</span>
-				<span className="max-label">{ this.props['max-label'] }</span>
-				<span className="max"><Number value={ this.props['max'] } /></span>
+				<div className={ styles.now } style={ this.getNowWidth() }>
+					<span className={ styles['now-label'] }>{ this.props['now-label'] }</span>
+					<span className={ styles['now-value'] }><Number value={ this.props['now'] } /></span>
+				</div>
+				<div className={ styles.max } style={ this.getMaxWidth() }>
+					<span className={ styles['max-label'] }>{ this.props['max-label'] }</span>
+					<span className={ styles['max-value'] }><Number value={ this.props['now'] } /></span>
+				</div>
 			</div>
 		);
 	}
