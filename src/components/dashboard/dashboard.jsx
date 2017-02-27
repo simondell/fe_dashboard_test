49528e4f5887fb5a7f478afcfa52a7046e851f5f
Simon Dell <simon@expressionworks.co.uk>
2017-02-27 01:18:57 +0000

    Removing redundant nesting #11

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,14 +1,20 @@
-import Metric from '../metric/metric';
+import Graph from '../graph/graph';
+import ProgressBar from '../progress_bar/progress_bar';
 import React from 'react';
 import Stats from '../stats/stats';
-import SalesSummary from '../sales_summary/sales_summary';
 import styles from './dashboard.css';
 
 export default class Dashboard extends React.Component {
 	render () {
 		return (
 			<main className={ styles.dashboard }>
-				<SalesSummary data={ this.props.data } />
+				<h1>Sales vs Target for Week 3</h1>
+				<Graph data={ this.props.data.graph } />
+				<ProgressBar
+					now={ this.props.data.sales.current }
+					now-label="Sales"
+					max={ this.props.data.sales.target }
+					max-label="Target" />
 				<Stats metrics={ this.props.data.metrics } />
 			</main>
 		);
